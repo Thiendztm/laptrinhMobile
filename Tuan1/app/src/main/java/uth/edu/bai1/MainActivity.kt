54a785f2065a7b40fb4297484a8d415ca0d47c42
Thiendztm <thiemtm0726@ut.edu.vn>
2025-10-14 00:43:30 +0700

    btvn

--- Tuan1/app/src/main/java/uth/edu/bai1/MainActivity.kt
+++ Tuan1/app/src/main/java/uth/edu/bai1/MainActivity.kt
@@ -4,44 +4,88 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import uth.edu.bai1.ui.theme.Bai1Theme
-
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Bai1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+    ){
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 32.dp, start = 8.dp, end = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    text = "<",
+                    style = MaterialTheme.typography.displaySmall
+                )
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Bai1Theme {
-        Greeting("Android")
+                Spacer(modifier = Modifier.weight(1f))
+
+                Text(
+                    text = "|",
+                    style = MaterialTheme.typography.displaySmall
+                )
+            }
+
+            Spacer(modifier = Modifier.height(100.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.rigbycat),
+                contentDescription = "rigby cat",
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.height(18.dp))
+
+            Text(
+                text = "Rigby Cat",
+                style = MaterialTheme.typography.displaySmall.copy(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+            Spacer(modifier = Modifier.height(6.dp))
+
+            Text(
+                text = "UTH, HCM",
+                style = MaterialTheme.typography.displaySmall.copy(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Light
+                )
+            )
+        }
     }
 }
